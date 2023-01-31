f8564cd464f7aa9c50cbb9007f77509803a99324
Mateus Gueler <mateus.gueler@gmail.com>
2023-01-31 14:47:31 -0300

    :pencil: feat:
    Create logic to create a new client using client infos

--- src/Controllers/ClientController.ts
+++ src/Controllers/ClientController.ts
@@ -4,19 +4,14 @@ import { ClientService } from '../Services/ClientService';
 export class ClientController {
 	private clientService = new ClientService();
 
-	create(req: Request, res: Response) {
+	async create(req: Request, res: Response) {
 		const {
 			name,
 			cpf,
 			birthday,
 		}: { name: string; cpf: string; birthday: string } = req.body;
 
-		const verifyCPF: boolean = this.clientService.verifyCpf(cpf);
-
-		if (!verifyCPF) {
-			return res.status(422).send('CPF invalido');
-		}
-
+		await this.clientService.verifyCpf(name, cpf, birthday);
 		return res.status(201).send({ name, cpf, birthday });
 	}
 }
--- src/Repositories/ClientRepoitory.ts
+++ src/Repositories/ClientRepoitory.ts
@@ -1,5 +1,18 @@
 import prisma from '../Database/prisma';
 
 export class ClientRepository {
-	create() {}
+	async create(name: string, cpf: string, birthday: string) {
+		const bithdateFormat = new Date(
+			new Date(birthday).toLocaleDateString('pt-BR')
+		);
+		return await prisma.clients.create({
+			data: { name, cpf, birthday: bithdateFormat },
+		});
+	}
+
+	async getClientBy(cpf: string) {
+		return await prisma.clients.findFirst({
+			where: { cpf },
+		});
+	}
 }
